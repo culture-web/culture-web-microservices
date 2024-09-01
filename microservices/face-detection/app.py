894dffb152955aad246faef626205506717a9619
nzixuan <nzixuan@gmail.com>
2024-09-01 22:21:36 +0800

    add image encoding and requirements

--- microservices/face-detection/app.py
+++ microservices/face-detection/app.py
@@ -1,7 +1,10 @@
 import face_detection
+import cv2
 from flask import Flask, request, jsonify
 import numpy as np
 from PIL import Image
+import base64
+
 
 app = Flask(__name__)
 
@@ -9,12 +12,27 @@ detector = face_detection.build_detector(
     "DSFDDetector", confidence_threshold=.5, nms_iou_threshold=.3)
 
 
+def encode_image(image):
+    """Encode image as a Base64 string."""
+    _, buffer = cv2.imencode('.jpg', image)
+    encoded_string = base64.b64encode(buffer).decode('utf-8')
+    return encoded_string
+
+
 def predict(image):
     img = np.asarray(image)
     faces = detector.detect(img)
     print(f'face found: {len(faces)}')
     faces = faces.astype(int)
-    return faces
+    detected_faces = []
+    if len(faces) > 0:
+        for x, y, w, h, prob in faces:
+            try:
+                face = img[y:h, x:w]
+                detected_faces.append(face)
+            except:
+                print('error occurred')
+    return detected_faces, faces
 
 
 @ app.route('/detect', methods=['POST'])
@@ -27,9 +45,9 @@ def classify():
     image = Image.open(image_file)
     if image.mode != 'RGB':
         image = image.convert('RGB')
-    prediction = predict(image)
-
-    return jsonify(prediction.tolist())
+    detected_faces, locations = predict(image)
+    encoded_faces = [encode_image(face) for face in detected_faces]
+    return jsonify({'faces': encoded_faces, 'locations': locations.tolist()})
 
 
 if __name__ == '__main__':
