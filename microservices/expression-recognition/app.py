576fc31206dc8b0c2e4a7e99ce992b8cf1c528eb
Zong Xian <48787208+kangzongxian@users.noreply.github.com>
2024-09-03 23:09:12 +0800

    Make expression detection
    Make the expression detection work

--- microservices/expression-recognition/app.py
+++ microservices/expression-recognition/app.py
@@ -1,5 +1,4 @@
 import cv2
-import face_detection
 from flask import Flask, request, jsonify
 from PIL import Image
 import tensorflow
@@ -8,44 +7,30 @@ import numpy as np
 app = Flask(__name__)
 model = tensorflow.keras.models.load_model('./kathakali.h5')
 
-detector = face_detection.build_detector(
-    "DSFDDetector", confidence_threshold=.5, nms_iou_threshold=.3)
 emotion = ['Anger', 'Love', 'Odious', 'Pitty',
            'Terrible', 'Peace', 'Comic', 'Heroic', 'Wonder']
 
 
 def predict(image):
     img = np.asarray(image)
-    faces = detector.detect(img)
-    print(f'face found: {len(faces)}')
-    faces = faces.astype(int)
-    # loop over all detected faces
-    accu = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    if len(faces) > 0:
-        for x, y, w, h, prob in faces:
-            try:
-                face = img[y:h, x:w]
-                eiei = cv2.resize(face, (100, 100))
-                cv2.imshow("Cropped Face", eiei)
-                new = cv2.resize(face, (32, 32))
-                new = new.reshape(1, 32, 32, 3)
-                new = new.astype('float32')
-                dummy = new.copy() / 255
-                pred = model.predict(dummy)
-                i = pred.squeeze()
-                print(face)
-                print(pred)
-                print(i)
-                for number in range(9):
-                    print(f'{emotion[number]}: {round(i[number] * 100, 2)} %')
-                print(f'This picture is {emotion[np.argmax(i)]}')
-                accu[np.argmax(i)] += 1
-            except:
-                print('error occurred')
-    return "test"
+    # Loop through the img and predict the face expression
+    try:
+        new = cv2.resize(img, (32, 32))
+        new = new.reshape(1, 32, 32, 3)
+        new = new.astype('float32')
+        dummy = new.copy() / 255
+        pred = model.predict(dummy)
+        i = pred.squeeze()
+        accu = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        for number in range(9):
+            accu[number] = round(i[number] * 100, 2)
+        prediction = emotion[np.argmax(i)]
+    except:
+        prediction = 'error occurred'
+        accu = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    return {'prediction': prediction, 'accuracy': accu}
 
-
-@ app.route('/classify', methods=['POST'])
+@ app.route('/expression', methods=['POST'])
 def classify():
     if 'image' not in request.files:
         return jsonify({'error': 'no image found'})
@@ -56,7 +41,7 @@ def classify():
     if image.mode != 'RGB':
         image = image.convert('RGB')
     prediction = predict(image)
-    return jsonify({'prediction': prediction})
+    return jsonify(prediction)
 
 
 if __name__ == '__main__':
